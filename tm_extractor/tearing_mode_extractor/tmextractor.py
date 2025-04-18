06d70c4f494ae2b4e45223c3c66bf6117b9da5ce
lry-free <lryaurora@gmail.com>
2025-04-18 16:18:24 +0800

    revise run_tm_extractor.py to TMExtractor

--- tm_extractor/tearing_mode_extractor/tmextractor.py
+++ tm_extractor/tearing_mode_extractor/tmextractor.py
@@ -110,8 +110,8 @@ class TMExtractor(BaseExtractor):
         # Raw signal tags to plot
         self.plot_raw_tags = config[self.JTEXT][self.PLOT_RAW_TAGS]
 
-    def load_config(self, config_file_path):
-        with open(config_file_path, 'r', encoding='utf-8') as f:  # 使用UTF-8编码
+    def load_config(self):
+        with open(self.config_file_path, 'r', encoding='utf-8') as f:  # 使用UTF-8编码
             return json.load(f)
     def extract_steps(self) -> List[Step]:
         """
